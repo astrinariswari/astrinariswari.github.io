5bef414e14b82327506534828cc21192b072ceb0
astrinariswari <107674311+astrinariswari@users.noreply.github.com>
2022-06-17 16:33:58 +0700

    Add files via upload

---
+++ text_mining/text_mining.R
@@ -0,0 +1,40 @@
+library(rtweet)
+library(ggplot2)
+library(dplyr)
+library(tm)
+library(wordcloud)
+
+get_token()
+
+
+rstat_tweets <- search_tweets(q="#KetahananPangan", n= 5000)
+
+head(rstat_tweets, n=5)
+textdata <- head(rstat_tweets$text)
+textdata <- rstat_tweets$text
+tweet_doc <- Corpus(VectorSource(textdata))
+
+# clean corpus
+tweet_doc <- tm_map(tweet_doc, content_transformer(tolower))
+tweet_doc <- tm_map(tweet_doc, removeNumbers)
+tweet_doc <- tm_map(tweet_doc, removeWords)
+tweet_doc <- tm_map(tweet_doc, removePunctuation)
+tweet_doc <- tm_map(tweet_doc, stripWhitespace)
+
+# convert corpus for futher manipulation
+dtm <- TermDocumentMatrix(tweet_doc)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m), decreasing = TRUE)
+d <- data.frame(frequency=v)
+d <- data.frame(word=names(v), frequency=v)
+head(d,10)
+
+
+# plot most frequent
+barplot(d[1:15,]$frequency, las=2, names.arg = d[1:15,]$word, col = "salmon", 
+        main = "Top 15 Most Frequent Words", ylab = "word frequencies" )
+
+# wordcloud
+set.seed(1234)
+wordcloud(words=d$word, freq = d$frequency, min.freq = 5, max.words = 1000, 
+          random.order = F, colors = brewer.pal(8, "Dark2"), rot.per = 0.3)
